46e8327cfade3750cf31d9400e9b2c701d377678
Hamza Akram <101792969+Hamzaakrammogul@users.noreply.github.com>
2022-08-24 14:24:11 +0500

    Update MyNftTest.js

--- test/MyNftTest.js
+++ test/MyNftTest.js
@@ -6,6 +6,7 @@ describe("Deployment", function(){
     let addr1;
     let addr2;
     let myNFTToken;
+    const zero_address = "0x0000000000000000000000000000000000000000";
     let name= "MyNFT";
     let symbol="MNFT";
     const baseURI = "https://api.mynft.com/tokens/";
@@ -28,27 +29,48 @@ describe("Deployment", function(){
   });
   describe("balanceOf",function(){
     it("check the balnce of owner", async function(){
-    for(i=1; i<=2; i++){
-    await myNFTToken.safeMint(owner.address);
+    for(i=0; i<=10; i++){
+        await myNFTToken.safeMint(owner.address);
         }
-    const balacnceOfOwner=await myNFTToken.balanceOf(owner.address);
-    expect(balacnceOfOwner).to.equal(2);
+        const balacnceOfOwner=await myNFTToken.balanceOf(owner.address);
+        expect(balacnceOfOwner).to.equal(11);
 });
+    it("Reversts if user trys to mint more token than the limit", async function(){
+    for(i=0; i>10; i++){
+        await expect(await myNFTToken.safeMint(addr1.address)).to.be.revertedWith
+        ("The minting limit has reached to its max, Sorry!");
+      }
+    });
+    it("Reverts if zero address try to mint", async function(){
+        await expect(myNFTToken.safeMint(zero_address)).to.be.reverted;
+  });
   });
   describe("ownerOf", function(){
     it("Shows the owner of token", async function(){
-         await myNFTToken.safeMint(owner.address);
-         await myNFTToken.safeMint(addr1.address);
-         expect(await myNFTToken.ownerOf(0)).to.equal(owner.address);
-         expect(await myNFTToken.ownerOf(1)).to.equal(addr1.address);
+        await myNFTToken.safeMint(owner.address);
+        await myNFTToken.safeMint(addr1.address);
+        expect(await myNFTToken.ownerOf(0)).to.equal(owner.address);
+        expect(await myNFTToken.ownerOf(1)).to.equal(addr1.address);
     });
+    
   });
-  describe("Transactional", function(){
-    it.only("Transfer the ownership of token", async function(){
+  describe("transferFrom", function(){
+    it("Transfer the ownership of token", async function(){
         await myNFTToken.safeMint(addr1.address);
         await myNFTToken.connect(addr1).transferFrom(addr1.address, addr2.address, 0);
         expect(await myNFTToken.balanceOf(addr2.address)).to.equal(1);
         expect(await myNFTToken.balanceOf(addr1.address)).to.equal(0);
     })
+    it("Reverts if the caller is not msg.sender", async function(){
+        await myNFTToken.safeMint(owner.address);
+        const attacker= await myNFTToken.connect(addr1);
+        await expect(attacker.transferFrom(addr1.address,addr2.address, 0))
+        .to.be.revertedWith("ERC721: caller is not token owner nor approved");
+    });
+    it("Reverts if the _to is the zero address", async function(){ 
+        await myNFTToken.safeMint(owner.address);
+        await expect(myNFTToken.transferFrom(owner.address, zero_address, 0))
+        .to.be.revertedWith("ERC721: transfer to the zero address");
+    });
   })
 });
